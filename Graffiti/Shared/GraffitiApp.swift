3707ea32975a02ed928f6b42c0bce0ad82925fe0
richardli03 <rli@olin.edu>
2021-12-27 12:09:24 -0500

    Created Graffiti App XCode workspace

---
+++ Graffiti/Shared/GraffitiApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraffitiApp.swift
+//  Shared
+//
+//  Created by Richard Li on 12/27/21.
+//
+
+import SwiftUI
+
+@main
+struct GraffitiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
