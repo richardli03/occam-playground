d4d73810a1681f6680bb8f743a9db116d5be2deb
richardli03 <rli@olin.edu>
2022-06-29 13:00:20 -0400

    created function to check for good data

--- invisible_map/rtabmap_tests/gt_data_analysis.py
+++ invisible_map/rtabmap_tests/gt_data_analysis.py
@@ -6,9 +6,29 @@ if it could be viable as ground truth data.
 import json
 import os
 
-RTABMAP_DATA_PATH = "poses6.g2o"
-OUTPUTED_JSON_PATH = "groundtruth.json"
+RTABMAP_DATA_PATH = "test_data/poses6.g2o"
+OUTPUTED_JSON_PATH = "gt_json/gt6.json"
 
+def check_data(new_data_entry):
+    """
+    The file containing all of the checks to ensure the processed data is useable
+
+    Args:
+        new_data_entry (list): A list of all necessary data (ID + Translation + POSE)
+
+    Returns:
+        Boolean: True if data is good, False if data is bad.
+    """
+    if len(new_data_entry) != 9:
+        print(f"ERROR in {new_data_entry}")
+        return False
+    
+    # odometry nodes are saved as markers as well, but with a negative ID.
+    if float(new_data_entry[1]) < 0:
+        print(f"Negative marker {new_data_entry[1]} detected and thrown")
+        return False
+    
+    return True
 
 # initialize empty list of tag poses
 tag_poses =[]
@@ -19,22 +39,16 @@ base = os.path.splitext(RTABMAP_DATA_PATH)[0]
 os.rename(RTABMAP_DATA_PATH, base + ".txt")
 RTABMAP_DATA_PATH = RTABMAP_DATA_PATH[:-4] + ".txt"
 
+# Handling data
 with open(RTABMAP_DATA_PATH,"r") as f:
     for line in f.readlines():
         new_data_entry = line.split()
-        if new_data_entry[0] == "MARKER":
+        
+        # Right now we only care about handling tags (aka MARKERs)
+        if new_data_entry[0] == "MARKER":   
             
-            # ignoring bad data
-            if len(new_data_entry) != 9:
-                print(f"ERROR in {new_data_entry}")
-                error = True
-            
-            # odometry nodes are saved as markers as well, but with a negative ID.
-            if float(new_data_entry[1]) < 0:
-                print(f"Negative marker {new_data_entry[1]} detected and thrown")
-                error = True
-            
-            if error == False:
+            # Only append if the data is good      
+            if check_data(new_data_entry):
                 tag_poses.append({"tag_id": int(new_data_entry[1]), "pose": [float(blah) for blah in new_data_entry[2:]]})
             
         else:   
